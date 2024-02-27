1ba62dbbdc84d3cc1fa190083e5aaf188734d01a
Shreeraj Shinde <shreerajs123@gmail.com>
2024-02-27 18:24:52 +0530

    Complete Signup Functionality Added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -13,6 +15,21 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
