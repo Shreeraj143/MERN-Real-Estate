053c1e65af1939e0c5b8c10a6299aaa9428460af
Shreeraj Shinde <shreerajs123@gmail.com>
2024-02-27 13:15:25 +0530

    Added database connection

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
